0e03960d40f5d9be8e2ca3337118980d3d2c8fee
Jetross <jetrossneri07@gmail.com>
2025-03-25 18:23:00 +0800

    done setting Payment controller through Stripe API

--- app/Http/Controllers/Api/PaymentController.php
+++ app/Http/Controllers/Api/PaymentController.php
@@ -3,9 +3,49 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use ErrorException;
 use Illuminate\Http\Request;
+use Stripe\Checkout\Session;
+use Stripe\Stripe;
 
 class PaymentController extends Controller
 {
-    //
+    public function payByStripe(Request $request)
+    {
+        Stripe::setApiKey(env('STRIPE_KEY'));
+
+        try {
+            $checkout_session = Session::create([
+                'line_items' => [[
+                    'price_data' => [
+                        'currency' => 'usd',
+                        'product_data' => [
+                            'name' => 'Meal Shop Product',
+                        ],
+                        'unit_amount' => $this->calculateOrderTotal($request->items),
+                    ],
+                    'quantity' => 1,
+                ]],
+                'mode' => 'payment',
+                'success_url' => $request->success_url,
+            ]);
+            return response()->json(['url' => $checkout_session->url]);
+        } catch (ErrorException $e) {
+            response()->json(['error' => $e->getMessage()]);
+        }
+    }
+
+    public function calculateOrderTotal($items)
+    {
+        $total = 0;
+        foreach ($items as $item) {
+            $total += $this->calculateTotal($item['price'], $item['quantity']);
+            return $total * 100;
+        }
+    }
+
+    public function calculateTotal($price, $quantity)
+    {
+        return $price * $quantity;
+    }
 }
