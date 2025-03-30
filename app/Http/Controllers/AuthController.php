351a9d04d35002be1bda3b6ddb834a0316581ada
Jetross <jetrossneri07@gmail.com>
2025-03-30 15:44:10 +0800

    added sanctum

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 
 class AuthController extends Controller
@@ -28,5 +29,28 @@ class AuthController extends Controller
             'message' => 'User registered successfully!',
             'user' => $user,
         ], 201);
+    }
+
+    public function login(Request $request)
+    {
+        // Validate the request
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+        // Attempt to log in the user
+        if (Auth::attempt($credentials)) {
+            $user = User::find($auth->id)->get();
+
+            return response()->json([
+                'message' => 'Login successful',
+                'user' => $user,
+                'token' => $token,
+            ], 200);
+        }
+
+        return response()->json([
+            'message' => 'Invalid credentials',
+        ], 401);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Api\PaymentController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController; //route for creating user
 
+
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
@@ -13,3 +14,4 @@ use App\Http\Controllers\AuthController; //route for creating user
 Route::get('products', [ProductController::class, 'index']);
 Route::post('pay/order', [PaymentController::class, 'payByStripe']);
 Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
