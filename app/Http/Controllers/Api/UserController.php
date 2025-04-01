0b9df1e265a5fe0cf5ce9d189d74f127cb39dd2d
Jetross <jetrossneri07@gmail.com>
2025-04-01 09:34:18 +0800

    getting UserDetails from database is working

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function getUserDetails($id)
+    {
+        // Fetch user details by ID
+        $user = User::find($id);
+
+        if (!$user) {
+            return response()->json(['message' => 'User not found'], 404);
+        }
+
+        return response()->json($user, 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\PaymentController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController; //route for creating user
+use App\Http\Controllers\Api\UserController; //route for getting user details
 
 
 // Route::get('/user', function (Request $request) {
@@ -15,3 +16,4 @@ Route::get('products', [ProductController::class, 'index']);
 Route::post('pay/order', [PaymentController::class, 'payByStripe']);
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::get('/user/{id}', [UserController::class, 'getUserDetails']);
