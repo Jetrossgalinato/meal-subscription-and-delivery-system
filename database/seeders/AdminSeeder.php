081311aa5a259b0a68374eb620240a217d6f1d74
Jetross <jetrossneri07@gmail.com>
2025-04-02 22:38:16 +0800

    added an admin account on the database

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Admin User',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('password'), // Replace 'password' with a secure password
+            'is_admin' => true, // Set is_admin to true
+        ]);
+    }
+}
