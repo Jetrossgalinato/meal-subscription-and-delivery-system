530ab4295d76b20069897d520be410e231e684b4
Jetross <jetrossneri07@gmail.com>
2025-04-02 20:48:03 +0800

    added a function on MealController

--- app/Http/Controllers/Api/MealController.php
+++ app/Http/Controllers/Api/MealController.php
@@ -4,8 +4,14 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Meal; // Import the Meal model
 
 class MealController extends Controller
 {
-    //
+    public function getMeals()
+    {
+        $meals = Meal::all();
+
+        return response()->json($meals);
+    }
 }
