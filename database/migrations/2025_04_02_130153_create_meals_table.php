b645ff7a239bbe317ca81542a41a2314ef1a5593
Jetross <jetrossneri07@gmail.com>
2025-04-02 21:07:15 +0800

    added meals migration

---
+++ database/migrations/2025_04_02_130153_create_meals_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('meals', function (Blueprint $table) {
+            $table->id();
+            $table->string('name'); // Meal name
+            $table->text('description'); // Meal description
+            $table->decimal('price', 8, 2); // Meal price
+            $table->string('image')->nullable(); // Meal image (optional)
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('meals');
+    }
+};
