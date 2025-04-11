972f83efb2144d6beaf06916e015c1faede0aee5
Jetross <jetrossneri07@gmail.com>
2025-04-11 22:08:41 +0800

    removed product api.php

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Http\Request;
-use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\PaymentController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController; //route for creating user
@@ -16,7 +15,6 @@ use App\Http\Controllers\CheckoutController;
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
-Route::get('products', [ProductController::class, 'index']);
 Route::post('pay/order', [PaymentController::class, 'payByStripe']);
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
